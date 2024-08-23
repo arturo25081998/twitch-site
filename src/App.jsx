d1dffaf40dfc9bd5f37bf7b26933aabe5f78cd7e
Arturo Juarez <ajrobles98@gmail.com>
2024-08-22 18:19:57 -0600

    feat : adding initial files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/navbar";
+
+import Body from "./components/body";
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Body></Body>
+    </>
+  );
+}
+
+export default App;
